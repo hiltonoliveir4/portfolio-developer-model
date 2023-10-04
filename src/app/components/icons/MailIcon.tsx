daced6427b80312d11c50b7c6a8832670d13406d
hiltonoliveir4 <hiltonoliveira@inforgeneses.com.br>
2023-10-04 11:53:35 -0300

    feat: add more icons

---
+++ src/app/components/icons/MailIcon.tsx
@@ -0,0 +1,15 @@
+export default function MailIcon() {
+  return (
+    <svg fill="#fe655c" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path d="M21,4H3A1,1,0,0,0,2,5V19a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V5A1,1,0,0,0,21,4ZM8.707,14.707l-2,2a1,1,0,0,1-1.414-1.414l2-2a1,1,0,0,1,1.414,1.414Zm10,2a1,1,0,0,1-1.414,0l-2-2a1,1,0,0,1,1.414-1.414l2,2A1,1,0,0,1,18.707,16.707ZM19.5,8.868l-7,4a1,1,0,0,1-.992,0l-7-4A1,1,0,1,1,5.5,7.132L12,10.849l6.5-3.717A1,1,0,1,1,19.5,8.868Z"></path>
+      </g>
+    </svg>
+  );
+}
