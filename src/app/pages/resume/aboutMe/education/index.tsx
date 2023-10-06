e01f05a8c73d1f59d3a51d17798cbec60007cd32
hiltonoliveir4 <hiltonoliveira@inforgeneses.com.br>
2023-10-06 08:30:30 -0300

    fix: border include theme dark and light

--- src/app/pages/resume/aboutMe/education/index.tsx
+++ src/app/pages/resume/aboutMe/education/index.tsx
@@ -1,11 +1,14 @@
 import EducationList from "@/app/constants/education";
+import { ThemeContext } from "@/app/providers/themeProvider";
+import { useContext } from "react";
 import EducationItem from "./educationItem";
 import "./style.scss";
 
 export default function Education() {
   const educationList = EducationList();
+  const { globalTheme } = useContext(ThemeContext);
   return (
-    <div className={`education-container`}>
+    <div className={`education-container ${globalTheme}`}>
       {educationList.map((education, index) => (
         <EducationItem
           key={index}
